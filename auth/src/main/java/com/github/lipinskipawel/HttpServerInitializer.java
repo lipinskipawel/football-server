f0c141aa93e018c99567a8815ac5f1b80bf4a278
robert_ostaszewski <ostaszewski.robert@hotmail.com>
2022-04-23 22:59:02 +0200

    5 hours of debugging can save 5 minutes of reading documentation

--- auth/src/main/java/com/github/lipinskipawel/HttpServerInitializer.java
+++ auth/src/main/java/com/github/lipinskipawel/HttpServerInitializer.java
@@ -4,8 +4,12 @@ import com.github.lipinskipawel.register.RegisterEntrypoint;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.cors.CorsConfig;
+import io.netty.handler.codec.http.cors.CorsConfigBuilder;
+import io.netty.handler.codec.http.cors.CorsHandler;
 
 final class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -17,6 +21,10 @@ final class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
         ChannelPipeline p = ch.pipeline();
         p.addLast(new HttpServerCodec());
         p.addLast(new HttpObjectAggregator(4096));
+        p.addLast(new CorsHandler(CorsConfigBuilder.forAnyOrigin()
+                .allowedRequestMethods(HttpMethod.POST)
+                .allowedRequestHeaders("username")
+                .build()));
         p.addLast(RegisterEntrypoint.getRegisterHandler());
         p.addLast(new ClosingHandler());
     }
