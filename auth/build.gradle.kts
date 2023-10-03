b77d19b407d5d3f28e04c93c0f4db9be6b9b442e
Pawel Lipinski <lipinskijob@gmail.com>
2023-10-03 19:52:12 +0200

    [auth] Remove netty dependency

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -1,4 +1,3 @@
 dependencies {
-    implementation("io.netty:netty-all:4.1.73.Final")
     implementation("io.javalin:javalin:5.6.2")
 }
--- auth/src/main/java/com/github/lipinskipawel/register/RegisterEntrypoint.java
+++ auth/src/main/java/com/github/lipinskipawel/register/RegisterEntrypoint.java
@@ -1,24 +1,11 @@
 package com.github.lipinskipawel.register;
 
 import com.github.lipinskipawel.api.QueryRegister;
-import io.netty.channel.ChannelHandler;
 
 import java.util.Optional;
 
-/**
- * This class holds method to obtain production ready implementation of the {@link RegisterHandler} class.
- */
 public final class RegisterEntrypoint {
     public static final Register register = new Register(new TokenGenerator());
-
-    /**
-     * Production ready RegisterHandler.
-     *
-     * @return register handler
-     */
-    public static ChannelHandler getRegisterHandler() {
-        return createRegister(register);
-    }
 
     /**
      * This method returns production ready implementation of {@link QueryRegister} interface.
@@ -33,15 +20,5 @@ public final class RegisterEntrypoint {
                 return Optional.empty();
             }
         };
-    }
-
-    /**
-     * Package private helper method for testing.
-     *
-     * @param register to inject into {@link RegisterHandler}
-     * @return custom register handler
-     */
-    static ChannelHandler createRegister(final Register register) {
-        return new RegisterHandler(register);
     }
 }
