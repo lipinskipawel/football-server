a15dee62b2ebd5e0ec636562890989774c98f2ec
Pawel Lipinski <lipinskijob@gmail.com>
2023-10-24 19:10:05 +0200

    Configure logging for the server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     `jvm-test-suite`
-    id("com.github.johnrengelman.shadow") version("7.1.2")
+    id("com.github.johnrengelman.shadow") version ("7.1.2")
 }
 
 dependencies {
@@ -11,12 +11,12 @@ dependencies {
     implementation("io.netty:netty-all:4.1.73.Final")
     implementation("com.google.code.gson:gson:2.8.8")
     implementation("com.github.lipinskipawel:game-engine:5.0.0")
-    // currently, org.slf4j:slf4j-api:2.0.0-alpha5 is failing https://issues.apache.org/jira/browse/LOG4J2-3139
-    implementation("org.slf4j:slf4j-api:1.8.0-beta4")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
-    // The log4j2 binding for slf4j https://logging.apache.org/log4j/2.x/log4j-slf4j-impl/index.html
-    implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.17.1")
+
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("org.apache.logging.log4j:log4j-api:2.21.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.21.0")
+    // bindings between log4j2 and slf4j
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.21.0")
 }
 
 testing {
